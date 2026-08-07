ecee188c419727ee8ba23d9890888b58e88211f4
alfredoarevalo69 <alfredoarevalo69@gmail.com>
2026-08-06 20:44:07 -0500

    build: agregar adaptador de vercel para soporte SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sysarmortech.com',
+  output: 'server', // <--- OBLIGATORIO: Habilita Serverless / SSR
+
   redirects: {
     '/servicios': '/#servicios',
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
